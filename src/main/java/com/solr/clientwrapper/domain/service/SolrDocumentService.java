58df9d2b3c22a11c0d76e61b250e302bfa0374f1
Karthik Pillai <52003038+karthik261099@users.noreply.github.com>
2022-01-07 20:05:19 +0530

    Input Document Parser Using Schema on Client Library

--- src/main/java/com/solr/clientwrapper/domain/service/SolrDocumentService.java
+++ src/main/java/com/solr/clientwrapper/domain/service/SolrDocumentService.java
@@ -1,5 +1,11 @@
 package com.solr.clientwrapper.domain.service;
 
+import com.solr.clientwrapper.domain.dto.solr.SolrResponseDTO;
+import com.solr.clientwrapper.domain.port.api.SolrDocumentServicePort;
+import com.solr.clientwrapper.domain.utils.DocumentParserUtil;
+import com.solr.clientwrapper.domain.utils.MicroserviceHttpGateway;
+import org.apache.solr.common.SolrInputDocument;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,9 +13,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.solr.clientwrapper.domain.dto.solr.SolrResponseDTO;
-import com.solr.clientwrapper.domain.port.api.SolrDocumentServicePort;
-import com.solr.clientwrapper.domain.utils.MicroserviceHttpGateway;
+import java.util.Iterator;
+import java.util.Map;
 
 @Service
 @Transactional
@@ -25,6 +30,67 @@ public class SolrDocumentService implements SolrDocumentServicePort {
 
 		SolrResponseDTO solrResponseDTO = new SolrResponseDTO(collectionName);
 
+		Map<String, Map<String, Object>> schemaKeyValuePair= DocumentParserUtil.getSchemaOfCollection(baseMicroserviceUrl,collectionName);
+		if(schemaKeyValuePair == null){
+			String message="Unable to get the Schema. Please check the collection name again!";
+			log.debug(message);
+			solrResponseDTO.setMessage(message);
+			solrResponseDTO.setStatusCode(400);
+			return solrResponseDTO;
+		}
+
+		JSONArray payloadJSONArray=null;
+		try {
+			payloadJSONArray = new JSONArray(payload);
+		}catch (Exception e){
+			//TRY BY REMOVING THE QUOTES FROM THE STRING
+			try{
+				payload=payload.substring(1, payload.length() - 1);
+				payloadJSONArray = new JSONArray(payload);
+			}catch (Exception e1){
+				log.debug(e.toString());
+				log.debug(e1.toString());
+
+				String message="Invalid input JSON array of document.";
+				log.debug(message);
+				solrResponseDTO.setMessage(message);
+				solrResponseDTO.setStatusCode(400);
+				return solrResponseDTO;
+			}
+		}
+
+		//TO CHECK IF THE INPUT DOCUMENT SATISFIES THE SCHEMA
+		for(int i=0;i<payloadJSONArray.length();i++){
+
+			JSONObject jsonSingleObject= (JSONObject) payloadJSONArray.get(i);
+
+			DocumentParserUtil.DocumentSatisfiesSchemaResponse documentSatisfiesSchemaResponse =
+					DocumentParserUtil.isDocumentSatisfySchema(schemaKeyValuePair,jsonSingleObject);
+
+			if(documentSatisfiesSchemaResponse.isObjectSatisfiesSchema()){
+
+				SolrInputDocument solrInputSingleDocument=new SolrInputDocument();
+
+				Iterator<String> itr = jsonSingleObject.keySet().iterator();
+				while (itr.hasNext())
+				{
+					String payloadJsonObjectKey = itr.next();
+					Object payloadJsonObjectValue = jsonSingleObject.get(payloadJsonObjectKey);
+
+					solrInputSingleDocument.addField(payloadJsonObjectKey,payloadJsonObjectValue);
+				}
+
+			}else{
+				//ERROR IN A DOCUMENT STRUCTURE
+				solrResponseDTO.setMessage(documentSatisfiesSchemaResponse.getMessage());
+				solrResponseDTO.setMessage("The JSON input document in the array doesn't satisfy the Schema. Error: "+ documentSatisfiesSchemaResponse.getMessage());
+				solrResponseDTO.setStatusCode(400);
+				return solrResponseDTO;
+			}
+		}
+
+
+		//IF THE FLOW HAS REACHED HERE, IT MEANS THE INPUT JSON DOCUMENT SATISFIES THE SCHEMA.
 		MicroserviceHttpGateway microserviceHttpGateway = new MicroserviceHttpGateway();
 
 		String url=baseMicroserviceUrl + "/documents/" + collectionName;
