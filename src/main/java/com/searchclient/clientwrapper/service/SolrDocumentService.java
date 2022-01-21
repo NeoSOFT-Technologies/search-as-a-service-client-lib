b0cf13e264877062dda1792ba5f6aff6b9f32db1
skdhitman <piyushojha.skd@gmail.com>
2022-01-21 14:16:38 +0530

    Removed unused import

--- src/main/java/com/searchclient/clientwrapper/service/SolrDocumentService.java
+++ src/main/java/com/searchclient/clientwrapper/service/SolrDocumentService.java
@@ -10,7 +10,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.searchclient.clientwrapper.domain.dto.solr.SolrResponseDTO;
 import com.searchclient.clientwrapper.domain.port.api.SolrDocumentServicePort;
