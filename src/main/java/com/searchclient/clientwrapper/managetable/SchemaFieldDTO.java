cfba78c8aba199b607fae99a5b945bb91b8cc3c0
ojahagirdar <jahagirdaromkar@gmail.com>
2022-02-11 12:33:48 +0530

    Enum Remove

--- src/main/java/com/searchclient/clientwrapper/managetable/SchemaFieldDTO.java
+++ src/main/java/com/searchclient/clientwrapper/managetable/SchemaFieldDTO.java
@@ -1,7 +1,5 @@
 package com.searchclient.clientwrapper.managetable;
 
-import com.searchclient.clientwrapper.infrastructure.Enum.SchemaFieldType;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -14,7 +12,7 @@ import lombok.NoArgsConstructor;
 public class SchemaFieldDTO {
 
 	String name;
-	SchemaFieldType type;
+	String type;
 	String default_;
 	boolean isRequired;
 	boolean isFilterable;
