f4134f2c019c16d447c673a0cbb0cb9d7fa3d274
gurpreet1101 <mailtogurpreet1991@gmail.com>
2022-03-11 15:14:24 +0530

    Resolving 1 bug found by sonar cloud

--- src/main/java/com/searchclient/clientwrapper/domain/error/RestControllerAdvice.java
+++ src/main/java/com/searchclient/clientwrapper/domain/error/RestControllerAdvice.java
@@ -116,7 +116,7 @@ public class RestControllerAdvice {
 		if(exception.getCause() instanceof NumberFormatException) {
 			//NumberFormatException ex = (NumberFormatException)exception.getCause();
 			fieldName = exception.getName();
-			requiredType = exception.getRequiredType().getName();
+			requiredType = (null != exception.getRequiredType().getName())?exception.getRequiredType().getName():"";
 		}
 		return frameRestApiException(new RestApiError(HttpStatus.BAD_REQUEST, fieldName+" must be of type "+requiredType));
 	}
