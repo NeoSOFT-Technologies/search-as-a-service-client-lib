92e8b0991a465f2c875a2df62f5d84710bba8ac9
gurpreet1101 <mailtogurpreet1991@gmail.com>
2022-03-14 13:15:59 +0530

    Code Refinement

--- src/main/java/com/searchclient/clientwrapper/domain/error/RestControllerAdvice.java
+++ src/main/java/com/searchclient/clientwrapper/domain/error/RestControllerAdvice.java
@@ -116,7 +116,8 @@ public class RestControllerAdvice {
 		if(exception.getCause() instanceof NumberFormatException) {
 			//NumberFormatException ex = (NumberFormatException)exception.getCause();
 			fieldName = exception.getName();
-			requiredType = (null != exception.getRequiredType() && null != exception.getRequiredType().getName())?exception.getRequiredType().getName():"";
+			if(null != exception.getRequiredType() && null != exception.getRequiredType().getName())
+				requiredType = exception.getRequiredType().getName();
 		}
 		return frameRestApiException(new RestApiError(HttpStatus.BAD_REQUEST, fieldName+" must be of type "+requiredType));
 	}
