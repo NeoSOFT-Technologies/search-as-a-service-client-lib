e19ac1bf58b5b7c21ba0d78f2b48e4fd21181cf8
YashC2410 <yashchowdhary2000@gmail.com>
2022-05-26 15:45:31 +0530

    Removed Unwanted Exception Class

--- src/main/java/com/searchclient/clientwrapper/domain/error/JwtAuthenticationFailureException.java
+++ src/main/java/com/searchclient/clientwrapper/domain/error/JwtAuthenticationFailureException.java
@@ -1,6 +1,5 @@
 package com.searchclient.clientwrapper.domain.error;
 
-import org.springframework.stereotype.Component;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,7 +9,6 @@ import lombok.EqualsAndHashCode;
 @Data
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = false)
-@Component
 public class JwtAuthenticationFailureException extends RuntimeException{
 	private static final long serialVersionUID = 1L;
 	
--- src/main/java/com/searchclient/clientwrapper/domain/error/MicroserviceConnectionException.java
+++ src/main/java/com/searchclient/clientwrapper/domain/error/MicroserviceConnectionException.java
@@ -1,16 +1,13 @@
 package com.searchclient.clientwrapper.domain.error;
 
-import org.springframework.stereotype.Component;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-
 @Data
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = false)
-@Component
 public class MicroserviceConnectionException extends RuntimeException{
 	private static final long serialVersionUID = 1L;
 	
